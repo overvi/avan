446aab85e7b1158451d7b3dd311e5d3e5c2bfe2b
overvi <aliwebdevf@gmail.com>
2025-06-06 02:44:58 +0330

    a

---
+++ src/assets/js/home.js
@@ -0,0 +1,129 @@
+import { gsap } from "gsap";
+import { MorphSVGPlugin } from "gsap/MorphSVGPlugin";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+import Splide from "@splidejs/splide";
+
+gsap.registerPlugin(MorphSVGPlugin, ScrollTrigger);
+
+const toggleBtn = document.getElementById("mobile-nav-toggle");
+const nav = document.getElementById("mobile-nav");
+const backdrop = document.getElementById("mobile-nav-backdrop");
+
+let navOpen = false;
+
+function openNav() {
+  nav.classList.remove("opacity-0", "pointer-events-none");
+  nav.classList.add("opacity-100", "pointer-events-auto");
+
+  backdrop.classList.remove("opacity-0", "pointer-events-none");
+  backdrop.classList.add("opacity-100", "pointer-events-auto");
+
+  toggleBtn.classList.add("toggled");
+  toggleBtn.parentElement.classList.add("has-toggled");
+
+  navOpen = true;
+}
+
+function closeNav() {
+  nav.classList.remove("opacity-100", "pointer-events-auto");
+  nav.classList.add("opacity-0", "pointer-events-none");
+
+  backdrop.classList.remove("opacity-100", "pointer-events-auto");
+  backdrop.classList.add("opacity-0", "pointer-events-none");
+
+  toggleBtn.classList.remove("toggled");
+  toggleBtn.parentElement.classList.remove("has-toggled");
+
+  navOpen = false;
+}
+
+toggleBtn.addEventListener("click", (e) => {
+  e.stopPropagation();
+  navOpen ? closeNav() : openNav();
+});
+
+backdrop.addEventListener("click", (e) => {
+  if (navOpen && !nav.contains(e.target) && e.target !== toggleBtn) {
+    closeNav();
+  }
+});
+
+const groups = document.querySelectorAll(".circle-group");
+
+const tl = gsap.timeline({
+  repeat: -1,
+  repeatRefresh: true,
+  paused: true,
+});
+
+groups.forEach((group) => {
+  const circle = group.querySelector(".circle");
+  const highlights = group.querySelectorAll(".highlight");
+
+  tl.to(
+    circle,
+    {
+      duration: 0.6,
+      attr: {
+        r: () => {
+          const isLarge = Math.random() > 0.5;
+
+          highlights.forEach(
+            (h) => (h.style.opacity = isLarge ? h.getAttribute("opacity") : "0")
+          );
+          return isLarge ? 6 : 3;
+        },
+      },
+    },
+    0
+  );
+});
+
+ScrollTrigger.create({
+  trigger: "#lines",
+  start: "top center",
+  end: "bottom center",
+  onEnter: () => tl.play(),
+  onLeave: () => tl.pause(),
+  onEnterBack: () => tl.play(),
+  onLeaveBack: () => tl.pause(),
+});
+
+new Splide(".splide", {
+  direction: "rtl",
+  padding: { right: "15%", left: "15%" },
+  pagination: false,
+  mediaQuery: "min",
+  arrows : false,
+  perPage: 1,
+  focus: "center",
+  breakpoints: {
+    768: {
+      padding: { right: "30%", left: "30%" },
+      perPage: 1,
+      arrows : true
+    },
+  },
+}).mount();
+
+document.addEventListener("DOMContentLoaded", function () {
+  const accordionButtons = document.querySelectorAll("[data-accordion-target]");
+
+  accordionButtons.forEach((btn) => {
+    btn.addEventListener("click", () => {
+      const contentId = btn.getAttribute("data-accordion-target");
+      const contentEl = document.getElementById(contentId);
+      const isExpanded = btn.getAttribute("aria-expanded") === "true";
+
+      btn.setAttribute("aria-expanded", isExpanded ? "false" : "true");
+      btn.parentElement.classList.toggle("is-expanded")
+
+
+      if (isExpanded) {
+        contentEl.classList.add("hidden");
+      } else {
+        contentEl.classList.remove("hidden");
+      }
+    });
+  });
+});
---
+++ webpack.config.cjs
@@ -0,0 +1,81 @@
+const path = require("path");
+const CssMinimizerPlugin = require("css-minimizer-webpack-plugin");
+const HtmlBundlerPlugin = require("html-bundler-webpack-plugin");
+const TerserPlugin = require("terser-webpack-plugin");
+
+module.exports = {
+  entry: {
+    home: "./src/assets/js/home.js",
+  },
+
+  plugins: [
+    new HtmlBundlerPlugin({
+      entry: {
+        "/index": "./src/index.html",
+        "/step-1/index" : "./src/step-1/index.html"
+      },
+      js: {
+        filename: "js/[name].[contenthash:8].js",
+      },
+      css: {
+        filename: "css/[name].[contenthash:8].css",
+      },
+    }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.(mp4|webm|ogg)$/,
+        type: "asset/resource",
+        generator: {
+          filename: "assets/videos/[name][ext][query]",
+        },
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/,
+        type: "asset/resource",
+        generator: {
+          filename: "fonts/[name][ext]",
+        },
+      },
+
+      {
+        test: /\.css$/i,
+        use: ["css-loader", "postcss-loader"],
+      },
+
+      {
+        test: /\.(png|svg|jpg|jpeg|gif|webp|avif)$/i,
+        type: "asset/resource",
+        generator: {
+          filename: "assets/img/[name].[hash:8][ext]",
+        },
+      },
+    ],
+  },
+  devServer: {
+    static: "./dist",
+    hot: true,
+  },
+
+  resolve: {
+    extensions: [".tsx", ".ts", ".js", ".esm.js"],
+
+    alias: {
+      "@assets": path.join(__dirname, "./src/assets"),
+    },
+  },
+  output: {
+    filename: "js/[name].js",
+    path: path.resolve(__dirname, "dist"),
+    publicPath: "/",
+    clean: true,
+  },
+
+  optimization: {
+    runtimeChunk: "single",
+
+    minimizer: [new CssMinimizerPlugin(), new TerserPlugin()],
+  },
+  mode: "development",
+};
